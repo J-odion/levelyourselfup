c300793294d5fdf0e5bd4ba3d5c1748f73e3a626
J_odion <emmyjaff22@gmail.com>
2024-12-23 00:05:04 +0100

    out added

---
+++ out/_next/static/chunks/app/courses/page-12407f62d5ecb9c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[65,327,931,114,38],{6038:function(n,e,u){Promise.resolve().then(u.t.bind(u,5878,23))}},function(n){n.O(0,[878,971,117,744],function(){return n(n.s=6038)}),_N_E=n.O()}]);
