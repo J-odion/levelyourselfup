a9243de6f362c7924933c155f6289660089ac5a4
J_odion <emmyjaff22@gmail.com>
2024-12-24 05:25:44 +0100

    added mailchimp and fixed adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./component/Header";
 import FooterSection from "./component/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const figtree = localFont({
   src: "./fonts/Figtree.ttf",
@@ -19,12 +20,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3479358447222895"
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?
-client=ca-pub-3479358447222895"
-          crossorigin="anonymous"
-        ></script>
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
+        <Script id="mcjs" strategy="afterInteractive">
+          {`
+            !function(c,h,i,m,p){
+              m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+            }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/5ccd84da1698ec16db7fd21b5/fb2f9be663183b52d269afb07.js");
+          `}
+        </Script>
       </head>
       <body
         className={`${figtree.variable}  antialiased w-full bg-black text-white overflow-x-hidden`}
